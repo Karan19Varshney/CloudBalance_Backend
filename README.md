3308de061c95b75b24825507d9f5d2d9203a7691
Karan Varshney <kavarshney21@gmail.com>
2025-04-18 11:16:55 +0530

    3-phases commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CloudBalance_Backend
---
+++ src/main/java/com/example/CloudBalance_Backend/config/STSCredentialUtil.java
@@ -0,0 +1,35 @@
+package com.example.CloudBalance_Backend.config;
+
+
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
+import software.amazon.awssdk.services.sts.model.AssumeRoleResponse;
+import software.amazon.awssdk.services.sts.model.Credentials;
+
+import java.util.UUID;
+
+public class STSCredentialUtil {
+
+    public AwsCredentialsProvider getSessionCredentials(String roleArn) {
+        StsClient stsClient = StsClient.create();
+        AssumeRoleRequest roleRequest = AssumeRoleRequest.builder()
+                .roleArn(roleArn)
+                .roleSessionName("session-" + UUID.randomUUID())
+                .durationSeconds(3600)
+                .build();
+
+        AssumeRoleResponse roleResponse = stsClient.assumeRole(roleRequest);
+        Credentials credentials = roleResponse.credentials();
+
+        return StaticCredentialsProvider.create(
+                AwsSessionCredentials.create(
+                        credentials.accessKeyId(),
+                        credentials.secretAccessKey(),
+                        credentials.sessionToken()
+                )
+        );
+    }
+}
